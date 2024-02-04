e16e34306eba8deabc0cad7feadf57fd62eaad43
rwilgaard <rasmus@wilgaard.dk>
2024-02-04 11:13:17 +0100

    feat: Initial version

---
+++ alfredutils/alfredutils.go
@@ -0,0 +1,123 @@
+package alfredutils
+
+import (
+    "fmt"
+    "log"
+    "os"
+    "os/exec"
+    "time"
+
+    aw "github.com/deanishe/awgo"
+)
+
+type magicAuth struct {
+    Workflow *aw.Workflow
+    Account  string
+}
+
+func (a magicAuth) Keyword() string     { return "clearauth" }
+func (a magicAuth) Description() string { return "Clear credentials." }
+func (a magicAuth) RunText() string     { return "Credentials cleared!" }
+func (a magicAuth) Run() error          { return clearAuth(a.Workflow, a.Account) }
+
+func clearAuth(wf *aw.Workflow, keychainAccount string) error {
+    if err := wf.Keychain.Delete(keychainAccount); err != nil {
+        return err
+    }
+    return nil
+}
+
+func AddClearAuthMagic(wf *aw.Workflow, keychainAccount string) {
+    wf.Configure(aw.AddMagic(magicAuth{
+        Workflow: wf,
+        Account:  keychainAccount,
+    }))
+}
+
+func InitWorkflow(wf *aw.Workflow, cfg interface{}) error {
+    wf.Args()
+    if err := wf.Config.To(cfg); err != nil {
+        return err
+    }
+    return nil
+}
+
+func CheckForUpdates(wf *aw.Workflow) error {
+    updateJobName := "update"
+    if wf.UpdateCheckDue() && !wf.IsRunning(updateJobName) {
+        log.Println("Running update check in background...")
+        cmd := exec.Command(os.Args[0], updateJobName)
+        if err := wf.RunInBackground(updateJobName, cmd); err != nil {
+            return fmt.Errorf("Error starting update check: %s", err)
+        }
+    }
+
+    if wf.UpdateAvailable() {
+        wf.Configure(aw.SuppressUIDs(true))
+        wf.NewItem("Update Available!").
+            Subtitle("Press ⏎ to install").
+            Autocomplete("workflow:update").
+            Valid(false).
+            Icon(aw.IconInfo)
+    }
+
+    return nil
+}
+
+func HandleFeedback(wf *aw.Workflow) {
+    if wf.IsEmpty() {
+        wf.NewItem("No results found...").
+            Subtitle("Try a different query?").
+            Icon(aw.IconInfo)
+    }
+    wf.SendFeedback()
+}
+
+func LoadCache(wf *aw.Workflow, name string, out interface{}) error {
+    if wf.Cache.Exists(name) {
+        if err := wf.Cache.LoadJSON(name, out); err != nil {
+            return err
+        }
+    }
+    return nil
+}
+
+func RefreshCache(wf *aw.Workflow, name string, maxAge time.Duration) error {
+    cacheJobName := "cache"
+    if wf.Cache.Expired(name, maxAge) {
+        wf.Rerun(2)
+        if !wf.IsRunning(cacheJobName) {
+            cmd := exec.Command(os.Args[0], cacheJobName)
+            if err := wf.RunInBackground(cacheJobName, cmd); err != nil {
+                return err
+            }
+        } else {
+            log.Printf("%s job already running.", cacheJobName)
+        }
+
+        var cache []interface{}
+        err := LoadCache(wf, name, cache)
+        if err != nil {
+            return err
+        }
+
+        if len(cache) == 0 {
+            wf.NewItem("Refreshing cache…").
+                Icon(aw.IconInfo)
+            HandleFeedback(wf)
+        }
+    }
+    return nil
+}
+
+func HandleAuthentication(wf *aw.Workflow, keychainAccount string) {
+    _, err := wf.Keychain.Get(keychainAccount)
+    if err != nil {
+        wf.NewItem("You're not logged in.").
+            Subtitle("Press ⏎ to authenticate").
+            Icon(aw.IconInfo).
+            Arg("auth").
+            Valid(true)
+        HandleFeedback(wf)
+    }
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/rwilgaard/go-alfredutils
+
+go 1.21
+
+require github.com/deanishe/awgo v0.29.1
+
+require (
+	github.com/magefile/mage v1.15.0 // indirect
+	go.deanishe.net/env v0.5.1 // indirect
+	go.deanishe.net/fuzzy v1.0.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
